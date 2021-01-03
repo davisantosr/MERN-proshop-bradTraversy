c9b7de6110524af266d75f87497ce60d7c6a4e31
davisantosr <davi-rocha2@hotmail.com>
2021-01-02 23:20:05 -0300

    add font awesome cdn | add icon classes for cart and sign in

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ml-auto">
-                <Nav.Link href="/cart">Cart</Nav.Link>
-                <Nav.Link href="/login">Sign In</Nav.Link>
+                <Nav.Link href="/cart" className='fas fa-shopping-cart'>Cart</Nav.Link>
+                <Nav.Link href="/login" className='fas fa-users'>Sign In</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
