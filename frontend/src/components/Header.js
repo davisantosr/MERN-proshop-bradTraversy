1798c0194dd650375584e0783acbb3dc312e6231
davisantosr <davi-rocha2@hotmail.com>
2021-01-03 00:40:27 -0300

    add LinkContainer from react router component in Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,29 @@
 import React from 'react'
 
 import {Navbar, Nav, Container} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant='dark' collapseOnSelect expand="lg">
         <Container>
-          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand >Proshop</Navbar.Brand>
+          </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ml-auto">
-                <Nav.Link href="/cart" className='fas fa-shopping-cart'>Cart</Nav.Link>
-                <Nav.Link href="/login" className='fas fa-users'>Sign In</Nav.Link>
+                <LinkContainer to='/cart'>
+                  <Nav.Link>
+                    <i className='fas fa-shopping-cart'></i>Cart
+                  </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-users'></i>Sign In 
+                  </Nav.Link>
+                </LinkContainer> 
             </Nav>
           </Navbar.Collapse>
         </Container>
