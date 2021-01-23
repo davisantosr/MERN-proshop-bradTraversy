40fc00ceabacb49741d4741d2cfa3b4377bfdb70
davisantosr <davi-rocha2@hotmail.com>
2021-01-23 16:20:11 -0300

    custom error handling - middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import cors from 'cors'
-import products from './data/products.js'
 import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -16,14 +17,13 @@ app.get('/', (req, res) => {
   res.send('API is runing')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(product => product._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running ${process.env.NODE_ENV} on port ${PORT}`))
