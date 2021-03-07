deac9ad06d851893c5848a807ce1a4bcc832a289
davisantosr <davi-rocha2@hotmail.com>
2021-03-07 16:59:08 -0300

    CartScreen

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -14,12 +14,12 @@ export const cartReducer = (state = initialState, action) => {
     case CART_ADD_ITEM:
       const item = action.payload
 
-      const existItem = state.cartItem.find(x => x.product === item.product)
+      const existItem = state.cartItems.find(x => x.product === item.product)
 
       if(existItem){
         return {
           ...state,
-          cartItem: state.cartItems.map(x => x.product === existItem.product
+          cartItems: state.cartItems.map(x => x.product === existItem.product
             ? item : x)
         }
 
@@ -27,12 +27,12 @@ export const cartReducer = (state = initialState, action) => {
       else {
         return{
           ...state,
-          cartItem: [...state.cartItems, item]
+          cartItems: [...state.cartItems, item]
         }
       }
       return {
         ...state, 
-        cartItem: action.payload
+        cartItems: action.payload
       }
 
       default:
