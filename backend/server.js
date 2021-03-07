0a94907b20d76b34a5aa9580ba58b33561a61f6c
davisantosr <davi-rocha2@hotmail.com>
2021-03-07 18:28:24 -0300

    userController | authUser initial implement. | matchPassword Method | user Routes

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,32 @@
-import express from 'express'
-import cors from 'cors'
-import connectDB from './config/db.js'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import productRoutes from './routes/productRoutes.js'
-import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('API is runing')
-})
+});
 
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
-app.use(notFound)
+app.use(notFound);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running ${process.env.NODE_ENV} on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running ${process.env.NODE_ENV} on port ${PORT}`));
